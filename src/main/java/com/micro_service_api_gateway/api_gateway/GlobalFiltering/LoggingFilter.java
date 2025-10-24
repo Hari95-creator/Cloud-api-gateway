ce9608bac29ed1ea66251064afc4f28c4d4276cb
HARIKRISHNAN S G <harisgkrishnan@yahoo.com>
2025-10-24 15:37:10 +0530

    Logging Filter

---
+++ src/main/java/com/micro_service_api_gateway/api_gateway/GlobalFiltering/LoggingFilter.java
@@ -0,0 +1,24 @@
+package com.micro_service_api_gateway.api_gateway.GlobalFiltering;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        logger.info("Path Request Uri {}", exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+
+
+}
